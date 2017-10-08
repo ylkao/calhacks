270e85bbfe491e020df9aa38eccd9bd5f0e4e6ab
ylk-virtualbox <yiling.data@gmail.com>
2017-10-07 18:06:37 -0700

    just got images to display on page using ajax and pymongo

--- index.py
+++ index.py
@@ -1,43 +1,40 @@
-"""from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def main():
-    return render_template('home.html')
-
-
-   """
+import sys
+from flask_pymongo import pymongo #ylk
+from bson.json_util import dumps
+import requests  # to make GET request
+from bs4 import BeautifulSoup  # to parse the HTML response
+import re
 from flask import Flask, render_template
- 
+
 app = Flask(__name__)
+
+uri = 'mongodb://ashleychien:apricot12@ds147454.mlab.com:47454/cal-hacks' 
+
  
 @app.route('/<string:page_name>/')
 def render_static(page_name):
     return render_template('%s.html' % page_name)
- 
+
+@app.route('/api/v1/loadimages/<int:lastindex>/') 
+def load_images(lastindex):
+	client = pymongo.MongoClient(uri, connect=False)
+	db = client.get_default_database()
+	img = db['test']
+	print "sad"
+	return dumps(img.find({}))
+    #get mongodb results as a json array
+
+	# return arr
+
+#ashley's code
+"""
+def main(args):
+    client = pymongo.MongoClient(uri, connect=False)
+    db = client.get_default_database()
+    songs = db['test']
+    client.close()"""
+
+
 if __name__ == '__main__':
+    #main(sys.argv[1:]) #new
     app.run()
-    """
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return render_template('home.html')
-    
-@app.route('/about/')
-def about():
-    return render_template('about.html')
-@app.route('/exercise/')
-def exercise():
-    return render_template('exercise.html')
-@app.route('/food/')
-def food():
-    return render_template('food.html')
-@app.route('/games/')
-def games():
-    return render_template('games.html')
-@app.route('/vent/')
-def vent():
-    return render_template('vent.html')
-if __name__ == '__main__':
-    app.run(debug=True)"""
